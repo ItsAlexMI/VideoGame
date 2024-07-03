6987f39ba148753d8b6cca4f57f9352baf145dd0
CupOfCoffe <edwinjsa2021@gmail.com>
2024-07-02 22:33:53 -0600

    :memo: writing the docstring of scene_renderer.py

--- light.py
+++ light.py
@@ -1,13 +1,12 @@
 import glm
 
-
 class Light:
     def __init__(self, position=(10, 50, 50), color=(1, 1, 1)):
         self.position = glm.vec3(position)
         self.color = glm.vec3(color)
         self.direction = glm.vec3(0, 0, 0)
         # intensities
-        self.Ia = 0 * self.color  # ambient
+        self.Ia = 8 * self.color  # ambient
         self.Id = 0.001 * self.color  # diffuse
         self.Is = 0.04 * self.color  # specular
         # view matrix
--- scene_renderer.py
+++ scene_renderer.py
@@ -2,6 +2,9 @@
 
 class SceneRenderer:
     def __init__(self, app):
+        """
+        Initializes the SceneRenderer with the given 'app' object.
+        """
         self.app = app
         self.ctx = app.ctx
         self.mesh = app.mesh
@@ -11,24 +14,41 @@ class SceneRenderer:
         self.depth_fbo = self.ctx.framebuffer(depth_attachment=self.depth_texture)
 
     def render_shadow(self):
+        """
+        Render the shadow of each object in the scene.
+
+        This function clears the depth buffer and sets it as the current framebuffer.
+        Then, it iterates over each object in the scene and calls the `render_shadow()`
+        method on each object.
+        """
         self.depth_fbo.clear()
         self.depth_fbo.use()
         for obj in self.scene.objects:
             obj.render_shadow()
 
     def main_render(self):
+        """
+        Renders the main scene by iterating over each object in the scene and calling its `render` method.
+        Finally, it renders the skybox.
+        """
         self.app.ctx.screen.use()
         for obj in self.scene.objects:
             obj.render()
         self.scene.skybox.render()
 
     def render(self):
-        # self.scene.update()
-        # pass 1
+        """
+        Renders the scene by executing the shadow rendering and main rendering methods sequentially.
+        """
         self.render_shadow()
-        # pass 2
         self.main_render()
 
     def destroy(self):
+        """
+        Release the depth framebuffer object.
+
+        This method releases the depth framebuffer object associated with this instance.
+        It is used to clean up resources and prevent memory leaks.
+        """
         self.depth_fbo.release()
 
