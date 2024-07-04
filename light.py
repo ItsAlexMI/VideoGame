3d3df476eda4f6cdccff6c8bf5b22e340f535804
CupOfCoffe <edwinjsa2021@gmail.com>
2024-07-04 00:19:30 -0600

    :memo: Add comentaries to light.py

--- light.py
+++ light.py
@@ -2,6 +2,12 @@ import glm
 
 class Light:
     def __init__(self, position=(10, 50, 50), color=(1, 1, 1)):
+        """
+        Initialize the Light object with the specified position and color. 
+        Set the direction to the default (0, 0, 0). 
+        Calculate and assign the ambient, diffuse, and specular intensities. 
+        Compute the view matrix using the get_view_matrix method and assign it to m_view_light.
+        """
         self.position = glm.vec3(position)
         self.color = glm.vec3(color)
         self.direction = glm.vec3(0, 0, 0)
