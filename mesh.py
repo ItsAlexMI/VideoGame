d0392f795cb458f96fe10d4f982dfbee7247fba9
CupOfCoffe <edwinjsa2021@gmail.com>
2024-07-04 00:24:07 -0600

    :memo: Adding comentaries mesh.py

--- mesh.py
+++ mesh.py
@@ -4,10 +4,18 @@ from texture import Texture
 
 class Mesh:
     def __init__(self, app):
+        """
+        Initializes the Mesh object with the given 'app' object.
+        """
         self.app = app
         self.vao = VAO(app.ctx)
         self.texture = Texture(app)
 
     def destroy(self):
+        """
+        Destroys the mesh by releasing the associated vertex array object (VAO) and texture.
+
+        This function calls the `destroy()` method on the `vao` and `texture` attributes of the current instance.
+        """
         self.vao.destroy()
         self.texture.destroy()
